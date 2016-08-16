2cbd8d50f79156d9abc04d2bc61793ec5e9f6b49
Crazy7 <fangshanzhang@live.com>
2016-08-17 00:02:10 +0800

    Finish Basic function

--- Win10AppNetIsolator/Win10AppNetIsolator/MainWindow.xaml.cs
+++ Win10AppNetIsolator/Win10AppNetIsolator/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace Win10AppNetIsolator
 {
@@ -34,14 +35,55 @@ namespace Win10AppNetIsolator
 
 			Guid useless;
 
+			var appIdPrefix = @"HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Mappings\";
 			var items = mappingsKey.GetSubKeyNames().Select( _ => mappingsKey.OpenSubKey( _ ) )
-						.Select( _ => new MappingListItem { Key = _.Name, Value = _.GetValue( "DisplayName" )?.ToString() } )
+						.Select( _ => new MappingListItem { Key = _.Name.Substring( appIdPrefix.Length ), Value = _.GetValue( "DisplayName" )?.ToString() } )
 						.Where( _ => !_.Value.StartsWith( "@" ) )
 						.Where( _ => !_.Value.Contains( "-" ) || !Guid.TryParse( _.Value, out useless ) )
 						.OrderBy( _ => _.Value )
 						.ToList();
 
 			listBox.ItemsSource = items;
+		}
+
+		private void AppBoxSelectionChanged( object sender, SelectionChangedEventArgs e )
+		{
+			var selectedItem = listBox.SelectedItem as MappingListItem;
+			if ( selectedItem == null ) return;
+
+			commandBox.Text = $"CheckNetIsolation.exe loopbackexempt -a -p={selectedItem.Key}";
+		}
+
+		private void ExecuteBtnClick( object sender, RoutedEventArgs e )
+		{
+			try
+			{
+				button.IsEnabled = false;
+
+				var fileName = "CheckNetIsolation.exe";
+				var arguments = commandBox.Text.Substring( fileName.Length );
+
+				var startInfo = new ProcessStartInfo( fileName, arguments );
+				startInfo.RedirectStandardInput = true;
+				startInfo.RedirectStandardOutput = true;
+				startInfo.UseShellExecute = false;
+				startInfo.CreateNoWindow = true;
+
+				var p = Process.Start( startInfo );
+				p.OutputDataReceived += MainWindow_OutputDataReceived;
+
+				p.BeginOutputReadLine();
+				p.WaitForExit();
+			}
+			finally
+			{
+				button.IsEnabled = true;
+			}
+		}
+
+		private async void MainWindow_OutputDataReceived( object sender, DataReceivedEventArgs e )
+		{
+			await Dispatcher.InvokeAsync( () => outputBox.Text += e.Data + Environment.NewLine );
 		}
 	}
 }
